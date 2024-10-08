06aac217ce3d833b9205a6d4267d9e48027c20b5
Evgen-124 <Zhenya_panov_1994@mail.ru>
2024-10-08 19:03:55 +0300

    Практическое задание по теме "Переменные"

---
+++ variable.py
@@ -0,0 +1,6 @@
+homework_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / homework_count
+print(f"Курс: {course_name}, всего задач{homework_count}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_task:.3f} часа.")
+
